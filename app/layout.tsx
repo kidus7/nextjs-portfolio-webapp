95e9b64df0ba7c9c6bed14b9941f40c7beb0163a
kidus7 <kidusanteneh7@gmail.com>
2025-02-17 06:49:36 +0300

    changed icons loc

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Mignot Yirsaw" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/fav.png" />
       </head>
       <body className={inter.className}>
         {children}
