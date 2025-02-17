3afb674023f41c9e04ffd71264447af65e6e1206
kidus7 <kidusanteneh7@gmail.com>
2025-02-17 04:42:09 +0300

    included speed insights for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={inter.className}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
